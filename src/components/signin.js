71d22dbb9ad77fe9f996bb1d86f99a98ad4b7fd9
SahadevDahit <sahadevdahit123@gmail.com>
2021-10-04 19:57:42 +0545

    making-responsive

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,28 +1,34 @@
 import React from 'react'
-import {Form,Button} from 'react-bootstrap'
+import {Form,Button,Row,Col} from 'react-bootstrap'
 const signin = () => {
     return (
         <>
-           <Form className='mt-4' >
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label style={{ fontWeight: 'bold' }}>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+       
+       <Form className='mt-1.5'>
+       
+        <Row className="mb-3">
+          <Form.Group controlId="formGridEmail">
+            <Form.Label style={{ fontWeight: 'bold' }}>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label style={{ fontWeight: 'bold' }}>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+          <Form.Group controlId="formGridPassword">
+            <Form.Label style={{ fontWeight: 'bold' }}>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+        </Row>
+
+        <Form.Group controlId="formGridPassword">
+          <Form.Label style={{ fontWeight: 'bold' }}> Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Confirm Password" />
+        </Form.Group>
+
+
+
+        <Button variant="primary" type="submit" className='mt-2'>
+          Submit
+        </Button>
+      </Form>
            
         </>
     )
