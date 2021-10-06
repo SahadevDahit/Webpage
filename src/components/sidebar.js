d9ea0c5541629ef8cccdf655eda44c0d155eda5e
SahadevDahit <sahadevdahit123@gmail.com>
2021-10-06 23:32:48 +0545

    making-responsive

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -24,7 +24,7 @@ const Sidebar = () => {
             <div className="w3-sidebar w3-bar-block w3-card w3-animate-left" style={{ display: "none" }} id="mySidebar">
                 <button className="w3-bar-item w3-button w3-large bg-secondary fs-2"
                     onClick={w3_close}>Close &times;</button>
-                <div className='bg-danger h-100' > 
+                <div className='bg-danger h-100 select' > 
                     <button className="w3-bar-item w3-button fs-4"  onClick={()=>{window.scrollTo(0, 600);}}>Introduction</button>
                     <button className="w3-bar-item w3-button fs-4" onClick={()=>window.scrollTo(0,930)}>Installation</button>
                     <button className="w3-bar-item w3-button fs-4" onClick={()=>window.scrollTo(0,1150)}>Pre-requisties</button>
@@ -36,7 +36,7 @@ const Sidebar = () => {
                     <button className="w3-bar-item w3-button fs-4" onClick={()=>window.scrollTo(0,3800)}>Students Records</button>
                     <button className="w3-bar-item w3-button fs-4" onClick={()=>window.scrollTo(0,4300)}>Teachers Records</button>
                     <button className="w3-bar-item w3-button fs-4" onClick={()=>window.scrollTo(0,5000)}>Staff Records</button>
-                    <button className="w3-bar-item w3-button fs-4" onClick={()=>window.scrollTo(0,5700)}>Issuing/Depositing Books</button>
+                    <button className="w3-bar-item w3-button fs-4" onClick={()=>window.scrollTo(0,5700)}>Issue/Deposit</button>
  
                 </div> 
             </div>
